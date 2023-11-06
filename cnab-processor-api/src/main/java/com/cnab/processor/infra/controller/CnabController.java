670dcb1bb4c8a48728a9f7815b3b66d8f9480d90
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-11-06 14:48:44 -0300

    🐛 fix: add cors

--- cnab-processor-api/src/main/java/com/cnab/processor/infra/controller/CnabController.java
+++ cnab-processor-api/src/main/java/com/cnab/processor/infra/controller/CnabController.java
@@ -1,22 +1,28 @@
 package com.cnab.processor.infra.controller;
 
-import com.cnab.processor.infra.services.CnabService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.cnab.processor.infra.services.CnabService;
 
 @RestController
 @RequestMapping("/cnab")
+@CrossOrigin(origins = "*")
 public class CnabController {
 
 	@Autowired
 	private CnabService service;
 
 	@PostMapping("/upload")
-	public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) 
-		throws Exception {
-			service.uploadFile(file);
-			return ResponseEntity.ok("Start processing file...");
+	public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file)
+			throws Exception {
+		service.uploadFile(file);
+		return ResponseEntity.ok("Start processing file...");
 	}
 }
